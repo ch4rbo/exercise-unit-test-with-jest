ac0465accc431a1a7fd1d91077a628d896ce4c94
ch4rbo <baueratctu@gmail.com>
2021-09-07 00:01:58 +0000

    conversion de mosca

--- app.js
+++ app.js
@@ -8,4 +8,24 @@ console.log(sum(7,3))
 
 // export the function to be used on other files 
 // (similar to the keyword `export` when using webpack)
-module.exports = { sum };
+
+
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar; 
+}
+       // tenemos que incluir la funcion en el exports para que sea exportada a otros archivos como test.js
+module.exports = { sum, fromEuroToDollar }
+
+function fromDollarToYen
+
+
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
